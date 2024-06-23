ef4fdb430b1925afedb92904be0df0c7997d5569
Jonathan DIki <jonathandiki@outlook.com>
2024-06-24 01:32:45 +0200

    add update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,7 +25,7 @@ const About: NextPage = () => {
                     Bonjour et bienvenue sur mon portfolio ! Je suis Jonathan
                     Diki, un développeur web basé à Paris avec une passion
                     débordante pour les nouvelles technologies. Depuis mon plus
-                    jeune âge, j'ai toujours été fasciné par la manière dont la
+                    jeune âge, jai toujours été fasciné par la manière dont la
                     technologie peut transformer notre monde et améliorer notre
                     quotidien
                     <br />
@@ -34,7 +34,7 @@ const About: NextPage = () => {
                     à EPSI-Paris, où je me spécialise en développement web et en
                     programmation. Mon expertise principale se concentre sur les
                     frameworks modernes tels que React Js et Angular, avec une
-                    solide connaissance en HTML, CSS et JavaScript. J'ai
+                    solide connaissance en HTML, CSS et JavaScript. Jai
                     également acquis des compétences en backend avec Node.js et
                     en gestion de bases de données avec MongoDB.
                   </p>
@@ -190,8 +190,8 @@ const About: NextPage = () => {
                           </h6>
                           <h4 className="timeline-title">Stage Developpeur Angular</h4>
                           <p className="timeline-text">
-                            Au sein de 3kles Consulting, j'ai été impliqué dans
-                            divers projets passionnants qui m'ont permis de
+                            Au sein de 3kles Consulting, jai été impliqué dans
+                            divers projets passionnants qui mont permis de
                             mettre en pratique et de perfectionner mes
                             compétences en développement front-end.
                             Notament: Le Développement de composants dynamiques, Création et
@@ -219,7 +219,7 @@ const About: NextPage = () => {
                           </h6>
                           <h4 className="timeline-title">Graphics Design</h4>
                           <p className="timeline-text">
-                            Depuis 2017, j'ai travaillé en tant que graphiste.
+                            Depuis 2017, jai travaillé en tant que graphiste.
                             Les logiciels que je maitrise pour le design
                             graphique sont Photoshop et Illustrator.
                           </p>
