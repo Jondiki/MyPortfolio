91e83c57d52738415bb905b917ca3ec7a2af0419
Jonathan  Diki <55284362+Jondiki@users.noreply.github.com>
2024-10-01 05:06:30 +0200

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,17 +3,8 @@ import type { NextPage } from "next";
 const About: NextPage = () => {
   return (
     <main>
-      <section className="about section" id="about">
-        <div className="container">
-          <div className="row">
-            <div className="section-title padd-15">
-              <h2>About Me</h2>
-           
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+
+      
     </main>
   );
 };
